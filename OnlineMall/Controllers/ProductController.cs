d1a16a85e5d194efffb4e3f3c8ab02d0f63c92fd
Martyna Zielinska <zielinska0@outlook.com>
2022-08-11 23:35:07 +0200

    Functionality for adding new products

--- OnlineMall/Controllers/ProductController.cs
+++ OnlineMall/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineMall.Areas.Identity.Data;
+using OnlineMall.Models;
 
 namespace OnlineMall.Controllers
 {
@@ -19,5 +20,25 @@ namespace OnlineMall.Controllers
 				View(await _context.Products.ToListAsync()) :
 				Problem("Entity set 'Products' is null.");
 		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,ShortName,FullName,Price,Size,Color,Description,ImgPath")] Product product)
+		{
+			product.ImgPath = "/img/marynarka1.webp";
+			if (ModelState.IsValid)
+			{
+				_context.Add(product);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(product);
+		}
+
 	}
 }
