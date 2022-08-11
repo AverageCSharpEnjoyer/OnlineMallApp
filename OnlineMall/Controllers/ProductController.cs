34d4679b0a1903b6064b25f13cbd235b3d8d11c9
Martyna Zielinska <zielinska0@outlook.com>
2022-08-12 00:06:12 +0200

    Functionality for editing products

--- OnlineMall/Controllers/ProductController.cs
+++ OnlineMall/Controllers/ProductController.cs
@@ -74,7 +74,62 @@ namespace OnlineMall.Controllers
 			return RedirectToAction(nameof(Index));
 		}
 
+		// GET: Invoices/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
 
+			var product = await _context.Products.FindAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+
+		// POST: Products/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,ShortName,FullName,Price,Size,Color,Description,ImgPath")] Product product)
+		{
+			if (id != product.Id)
+			{
+				return NotFound();
+			}
+
+			product.ImgPath = "/img/marynarka1.webp";
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(product);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ProductExists(product.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(product);
+		}
+
+		private bool ProductExists(int id)
+		{
+			return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
 
 	}
 }
