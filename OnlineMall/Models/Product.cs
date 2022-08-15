5e1aa864255bdd83c168cfb86a66693f910ce274
Martyna Zielinska <zielinska0@outlook.com>
2022-08-15 16:06:48 +0200

    Changes in prices

--- OnlineMall/Models/Product.cs
+++ OnlineMall/Models/Product.cs
@@ -1,11 +1,17 @@
-﻿namespace OnlineMall.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineMall.Models
 {
 	public class Product
 	{
 		public int Id { get; set; }
 		public string? ShortName { get; set; }
 		public string? FullName { get; set; }
-		public double Price { get; set; }
+        [Range(1,9999.99)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+		public decimal Price { get; set; }
 		public int Size { get; set; }
 		public string? Color { get; set; }
 		public string? Description { get; set; }
--- OnlineMall/Models/ShoppingCart.cs
+++ OnlineMall/Models/ShoppingCart.cs
@@ -1,8 +1,14 @@
-﻿namespace OnlineMall.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineMall.Models
 {
 	public class ShoppingCart
 	{
-		public double SumPrice { get; set; }
+		[Range(1, 9999.99)]
+		[DataType(DataType.Currency)]
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal SumPrice { get; set; }
 		public int SumQuantity { get; set; }
 		public List<Item> ListOfItems { get; set; }
 	}
